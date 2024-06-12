825b2679dfd119959ac9468408a0333032dac533
kuzzzma <75898540+kuz-z-zma@users.noreply.github.com>
2024-06-12 11:33:27 +0300

    Version update to indicate pre-release

--- paradigm/theme_description.php
+++ paradigm/theme_description.php
@@ -3,6 +3,6 @@
 // Zenphoto theme definition file
 $theme_description['name'] = 'Paradigm';
 $theme_description['author'] = 'kuzzzma (since v1.2, original version by Olivier FFRENCH (offrench)';
-$theme_description['version'] = '2.0';
-$theme_description['date'] = '2024-06-05';
+$theme_description['version'] = '2.1a';
+$theme_description['date'] = '2024-06-12';
 $theme_description['desc'] = gettext_th("A responsive, Bootstrap-based, highly customizable Zenphoto theme (with more than 100 options), optimized for SEO and social sharing.<br>Latest version is available on <a href=\"https://github.com/kuz-z-zma/paradigm\" title=\"Paradigm theme by kuzzzma\" rel=\"nofollow\">Github</a>."); ?>
